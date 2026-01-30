81aa8433d9adeb0f70df7adfe41c2a972baa3935
Mohamed eslam10 <mokee2008mes@gmail.com>
2026-01-30 19:20:02 +0200

    BottomHeader created

--- src/components/header/Bottomheader.jsx
+++ src/components/header/Bottomheader.jsx
@@ -1,7 +1,84 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { IoMdMenu } from "react-icons/io";
+import { IoMdArrowDropdown } from "react-icons/io";
+import { NavLink, Link } from 'react-router-dom';
+import { PiSignInBold } from "react-icons/pi";
+import { FaUserPlus } from "react-icons/fa6";
+
+
 
 export default function Bottomheader() {
+  const [categories, setCategories] = useState([]);
+  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    fetch('https://dummyjson.com/products/categories')
+      .then((res) => res.json())
+      .then((data) => {
+        setCategories(data);
+      
+      })
+  }, []);
+  const nav_list = [
+    { title: 'Home', link: '/' },
+    { title: 'About', link: '/about' },
+    { title: 'Accessories', link: '/accessories' },
+    { title: 'Blog', link: '/blog' },
+    { title: 'Contact', link: '/contact' }
+  ]
   return (
-    <div></div>
+    <div>
+      <div className='bg-sky-600 flex justify-around mt-4 h-10 items-center'>
+        <nav className=' flex gap-18 '>
+
+          {/* //categories nav list// */}
+          <div
+            className='flex items-center relative cursor-pointer gap-2 text-white'
+            onClick={() => { setOpen(!open) }}>
+
+            <IoMdMenu />
+            <p>Browse Category</p>
+            <IoMdArrowDropdown className={`transition-transform ${open ? `rotate-180 transition` : ``}`} />
+
+            <div className={`
+            absolute top-full  mt-3 flex flex-col left-0 bg-white text-gray-700 border-1 border-gray-400 
+            transition-all w-44 rounded duration-200 shadow-lg z-100 overflow-y-auto max-h-75
+            ${open ? `block ` : `hidden`}
+            `}
+
+
+            >
+              {open && (
+
+                categories.map((category) => {
+                  return <Link className='border-b border-gray-300 text-sm px-2 py-2 hover:bg-gray-100 w-full' to={category.slug} key={category.url} >{category.name}</Link>
+                })
+              )}
+
+            </div>
+          </div>
+          {/* //nav list// */}
+          <div className=' flex  text-white items-center justify-center'>
+            {
+              nav_list.map((item) => {
+                return (
+                  <NavLink to={item.link} key={item.title} className={({ isActive }) => `
+                  ${isActive ? `bg-sky-700` : `hover:bg-sky-700 ` } 
+                 flex items-center h-10 px-2 transition duration-100
+                  `} >{item.title}</NavLink>
+                )
+              })
+            }
+
+          </div>
+
+
+        </nav>
+        <div className=' flex text-white items-center justify-center text-xl ' >
+          <Link className='hover:bg-sky-700 px-2 h-10 flex items-center transition duration-100  ' to="/login" ><PiSignInBold /></Link>
+          <Link className='hover:bg-sky-700 px-2 h-10 flex items-center transition duration-100' to="/add" ><FaUserPlus /></Link>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/header/Topheader.jsx
+++ src/components/header/Topheader.jsx
@@ -7,7 +7,7 @@ import { FaSearch } from "react-icons/fa";
 export default function Topheader() {
     return (
         <div className="font-sans ">
-            <div className='flex justify-between px-5 mt-3 gap-5'>
+            <div className='flex justify-between px-5 mt-3 gap-15'>
                 <Link to="/" ><img className='w-20 sm:w-30' src="/img/logo.png" alt="logo" /></Link>
                 <form action="" className='flex relative ' >
                     <input id='search' className='peer outline-none border-1 border-sky-600   bg-[#f5f5f5] p-3 h-9 sm:h-10 w-36 sm:w-50 md:w-80 rounded-l-full placeholder:text-sm text-sm placeholder:p-1' type="text" name='search' placeholder='search for products' />
@@ -17,11 +17,11 @@ export default function Topheader() {
                 <div className='flex gap-4 mt-2'>
                     <div className='relative'>
                         <span className='text-xl sm:text-2xl'><FaRegHeart /></span>
-                        <span className='absolute -top-2 -right-2 text-white bg-sky-600 h-4 w-4 rounded-full flex items-center justify-center text-xs'>0</span>
+                        <span className='absolute -top-2 -right-2 text-white bg-sky-600 h-4 w-4 rounded-full flex items-center justify-center text-xs'>4</span>
                     </div>
                     <div className='relative'>
                         <span className='text-xl sm:text-2xl'><IoCartSharp /></span>
-                        <span className='absolute -top-2 -right-2 text-white bg-sky-600 h-4 w-4 rounded-full flex items-center justify-center text-xs'>0</span>
+                        <span className='absolute -top-2 -right-2 text-white bg-sky-600 h-4 w-4 rounded-full flex items-center justify-center text-xs'>3</span>
 
                     </div>
                 </div>
